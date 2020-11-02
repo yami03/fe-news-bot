5e1729282d78d841efe0ed2d8de5f0cc9abd5358
yami03 <seula00027@gmail.com>
2020-11-03 00:09:34 +0900

    fix: date

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -7,16 +7,17 @@ interface slackArgs {
 }
 
 export default async ({ data, url }: slackArgs) => {
-  const date = new Date();
-  const year = date.getUTCFullYear();
-  let month = String(date.getUTCMonth() + 1);
+  const today = new Date();
+  const year = today.getUTCFullYear();
+  const date = today.getDate();
+  let month = String(today.getUTCMonth() + 1);
   month = month.length > 1 ? month : `0${month}`;
 
   const message: any = {
     attachments: [],
   };
 
-  if (!data[data.length - 1].name.includes(`${year}-${10}`)) {
+  if (!data[data.length - 1].name.includes(`${year}-${10}`) || date > 7) {
     return;
   }
 
